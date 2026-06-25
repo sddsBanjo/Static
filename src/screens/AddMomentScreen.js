60d7a213c131c24b9d19caac824c81f1bde8ab91
SDDS Banjo <168935997+sddsBanjo@users.noreply.github.com>
2026-06-25 11:41:22 -0300

    > implementação do sistema de salvar momentos

--- src/screens/AddMomentScreen.js
+++ src/screens/AddMomentScreen.js
@@ -1,9 +1,59 @@
-import { View, Text } from 'react-native'
+import { useState } from 'react'
+import { View, Text, TextInput, Button } from 'react-native'
+import { addMoment } from '../database/database'
 
 export default function AddMomentScreen() {
+  const [title, setTitle] = useState('')
+  const [description, setDescription] = useState('')
+
+  async function handleSave() {
+    try {
+      await addMoment(
+        title,
+        description,
+        null,
+        null
+      )
+
+      setTitle('')
+      setDescription('')
+
+      console.log('Momento salvo!')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>➕ Novo Momento</Text>
+    <View style={{ padding: 20 }}>
+      <Text>Título</Text>
+      <TextInput
+        value={title}
+        onChangeText={setTitle}
+        placeholder="Nosso momento especial!"
+        style={{
+          borderWidth: 1,
+          marginBottom: 10,
+          padding: 10
+        }}
+      />
+
+      <Text>Descrição</Text>
+      <TextInput
+        value={description}
+        onChangeText={setDescription}
+        placeholder={"Conte um pouco sobre o momento..."}
+        style={{
+          borderWidth: 1,
+          marginBottom: 20,
+          padding: 10
+        }}
+      />
+
+      <Button
+        title="Salvar Momento"
+        onPress={handleSave}
+      />
     </View>
-  );
+  )
 }
--- src/screens/LibraryScreen.js
+++ src/screens/LibraryScreen.js
@@ -1,9 +1,38 @@
-import { View, Text } from 'react-native'
+import { useEffect, useState } from 'react'
+import { View, Text, FlatList } from 'react-native'
+
+import { getMoments } from '../database/database'
 
 export default function LibraryScreen() {
+  const [moments, setMoments] = useState([])
+
+  async function loadMoments() {
+    const data = await getMoments()
+    setMoments(data)
+  }
+  
+  useEffect(() => {
+    loadMoments()
+  }, [])
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>📍 Biblioteca de Momentos</Text>
-    </View>
+    <View style={{ flex: 1, padding: 20 }}>
+      <FlatList
+        data={moments}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              borderWidth: 1,
+              padding: 15,
+              marginBottom: 10
+            }}
+          >
+            <Text>{item.title}</Text>
+            <Text>{item.description}</Text>
+          </View>
+        )}
+    />
+  </View>
   );
 }
